6823094be063626e367d56ef4fb01c05f76230ac
Raunak Sett <sett.raunak@gmail.com>
2019-03-17 15:22:47 +0530

    added missing number

---
+++ leetCode/array/missingNumber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length * (nums.length + 1) / 2;
+        for (int i = 0; i < nums.length; i++) {
+            n -= nums[i];
+        }
+        return n;
+    }
+}
