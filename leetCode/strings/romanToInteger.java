3004b2a623f427d1eb874056cb820e0db7dbb9dd
Raunak Sett <sett.raunak@gmail.com>
2019-03-17 12:38:51 +0530

    roman to int update

--- leetCode/strings/romanToInteger.java
+++ leetCode/strings/romanToInteger.java
@@ -1,28 +1,43 @@
 class Solution {
     
-    public int getVal(Character s) {
-        if (s == 'I') return 1;
-        else if (s == 'V') return 5;
-        else if (s == 'X') return 10;
-        else if (s == 'L') return 50;
-        else if (s == 'C') return 100;
-        else if (s == 'D') return 500;
-        else if (s == 'M') return 1000;
-        return 0;
-    }
     public int romanToInt(String s) {
         
         int num = 0;
-        int i = 0;
-        for (i=0; i < s.length(); i++) {
-            int curr = getVal(s.charAt(i));
-            int next = (i + 1 < s.length()) ? getVal(s.charAt(i + 1)) : 0;
-            if (curr < next) {
-                num += (next - curr);
-                i = i + 1;
+        int prev = 0;
+        int curr = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I':
+                    curr = 1;
+                    break;
+                case 'V':
+                    curr = 5;
+                    break;
+                case 'X':
+                    curr = 10;
+                    break;
+                case 'L':
+                    curr = 50;
+                    break;
+                case 'C':
+                    curr = 100;
+                    break;
+                case 'D':
+                    curr = 500;
+                    break;
+                case 'M':
+                    curr = 1000;
+                    break;
+                default:
+                    curr = 0;
+                    break;
+            }
+            if (curr < prev) {
+                num -= curr;
             } else {
                 num += curr;
             }
+            prev = curr;
         }
         
         return num;
