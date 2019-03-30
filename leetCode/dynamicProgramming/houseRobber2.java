9d6f68146535d7283dabc4e802530c74460d6cae
patlanshantnu <shantnu.patlan@gmail.com>
2019-03-30 12:41:32 +0530

    house rob 2 leet

---
+++ leetCode/dynamicProgramming/houseRobber2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        return Math.max(rob(nums, 0, nums.length-1), rob(nums, 1, nums.length));
+    }
+    
+    public int rob(int[] nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        int temp;
+        for(int i=start; i<end; i++){
+            temp = prev2;
+            prev2 = Math.max(prev1, prev2);
+            prev1 = nums[i]+temp;
+        }
+        return Math.max(prev1, prev2);
+    }
+}
