b6d0cadc873260dec22eae65abb13fc40489fe2a
patlanshantnu <shantnu.patlan@gmail.com>
2019-03-30 12:34:58 +0530

    update house robber

--- leetCode/dynamicProgramming/houseRobber.java
+++ leetCode/dynamicProgramming/houseRobber.java
@@ -1,15 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 0) return 0;
-        int dp[] = new int[nums.length];
-        dp[0] = nums[0];
-        int max_sum = dp[0];
-        for (int i=1; i < nums.length; i++) {
-            dp[i] = nums[i];
-            if (i - 2 >= 0) dp[i] = Math.max(dp[i], dp[i-2] + nums[i]);
-            if (i - 3 >= 0) dp[i] = Math.max(dp[i], dp[i-3] + nums[i]);
-            if (max_sum < dp[i]) max_sum = dp[i];
+        int prev1 = 0, prev2 = 0;
+        int temp;
+        for(int i=0; i<nums.length; i++){
+            temp = prev2;
+            prev2 = Math.max(prev1, prev2);
+            prev1 = nums[i]+temp;
         }
-        return max_sum;
+        return Math.max(prev1, prev2);
     }
 }
