f1ee7f129b8089e011e8c85370f20d8127cdac59
patlanshantnu <shantnu.patlan@gmail.com>
2019-04-16 23:56:50 +0530

    bst itr leet

---
+++ leetCode/design/binsarySearchTreeIterator.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode node = root;
+        while(node != null){
+            stack.push(node);
+            node = node.left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode res = stack.pop();
+        TreeNode node = res.right;
+        while(node != null){
+            stack.push(node);
+            node = node.left;
+        }
+        return res.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.empty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
